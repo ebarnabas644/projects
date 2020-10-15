c745703dc82b3782c2d5ec9d6a92c8dd0bfb2dd2
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2020-10-16 00:01:23 +0200

    center

--- schoolpage/sites/loginsite/login.php
+++ schoolpage/sites/loginsite/login.php
@@ -13,25 +13,27 @@ header("location: ../../sites/adminsite/admin.php");
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 </head>
 <body>
-<div class="container justify-content-xl-center">
+<div class="container">
 		<h1 class="text-center">Saturnus</h1>
 		<hr align="center">
-	<div id="login" class="col-sm-8 col-lg-1 col-xl-3">
-		<form action="" method="post">
-			<table align="center">
-				<tr>
-					<td><label>Felhasználónév: </label></td>
-					<td><input id="name" name="username" placeholder="username" type="text"></td>
-				</tr>
-				<tr>
-					<td><label>Jelszó: </label></td>
-					<td><input id="password" name="password" placeholder="**********" type="password"></td>
-				</tr>
-				<tr align="center">
-					<td colspan="2"><input name="submit" type="submit" value=" Login "></td>
-				</tr>
-			</table>
-		</form>
+		<div class="row justify-content-center">
+			<div id="login" class="col-sm-8 col-lg-5 col-xl-3">
+				<form action="" method="post">
+					<table align="center">
+						<tr>
+							<td><label>Felhasználónév: </label></td>
+							<td><input id="name" name="username" placeholder="username" type="text"></td>
+						</tr>
+						<tr>
+							<td><label>Jelszó: </label></td>
+							<td><input id="password" name="password" placeholder="**********" type="password"></td>
+						</tr>
+						<tr align="center">
+							<td colspan="2"><input name="submit" type="submit" value=" Login "></td>
+						</tr>
+					</table>
+				</form>
+			</div>
 	</div>
 </div>
 </body>
