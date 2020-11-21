d6814b5683c02f5ea62967195c1f82ba4532baa6
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2020-11-21 23:30:58 +0100

    id fix

--- sites/adminproductsite/adminproductsite.php
+++ sites/adminproductsite/adminproductsite.php
@@ -52,7 +52,7 @@ require ($_SERVER['DOCUMENT_ROOT'] ."/databaseaccess/accessdatabase.php");
 	$product = new ProductController();
 	echo $product->PrepareProduct();
 	?>
-	<a href='../../sites/editsite/addproductsite.php?id=<?php $_GET['id'] ?>'>Hozzáadás</ad>
+	<a href='../../sites/editsite/addproductsite.php?id=<?php echo $_GET['id']; ?>'>Hozzáadás</ad>
 	<table>
 	<?php
 	echo $product->PrepareGrades();
