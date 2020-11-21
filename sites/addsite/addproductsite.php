a8329188d2bf1dfd0210e219684841edd7b024e6
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2020-11-22 00:48:38 +0100

    css path fix

--- sites/addsite/addproductsite.php
+++ sites/addsite/addproductsite.php
@@ -18,7 +18,7 @@ foreach ($gradearray as $key => $grade) {
 <html>
 <head>
 	<title>Saturnus napló</title>
-	<link rel="stylesheet" type="text/css" href="../../style.css">
+	<link rel="stylesheet" type="text/css" href="../../style/style.css">
 	<link rel="stylesheet" type="text/css" href="../../bootstrap/css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
