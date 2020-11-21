7b508b285c0cd1ea2c00bee9861ccd5b7d34bff8
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2020-11-22 00:09:16 +0100

    query fixed

--- sites/adminproductsite/adminproductcontroller.php
+++ sites/adminproductsite/adminproductcontroller.php
@@ -23,7 +23,7 @@
 			require ($_SERVER['DOCUMENT_ROOT'] ."/databaseaccess/accessdatabase.php");
 			$grade = new Grades();
 			$id=$_GET["id"];
-			$gradearray = $grade->GetGrades(pg_query($db_connectiontocatalog, "SELECT * FROM grades INNER JOIN phones ON phones.id = grades.studentid WHERE $id = id ORDER BY date desc"));
+			$gradearray = $grade->GetGrades(pg_query($db_connectiontocatalog, "SELECT * FROM grades INNER JOIN phones ON phones.id = grades.studentid WHERE $id = grades.studentid ORDER BY date desc"));
 			$result = "";
 			foreach($gradearray as $key => $grade){
 				$result = $result . "<tr>
