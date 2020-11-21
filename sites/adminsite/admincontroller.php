ef6e9761d988461cba307c2e6ea8e2d6d715e6ae
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2020-11-21 22:47:25 +0100

    href fix

--- sites/adminsite/admincontroller.php
+++ sites/adminsite/admincontroller.php
@@ -10,17 +10,17 @@
 			foreach ($phonearray as $key => $phone) {
 				$result=$result . "<div class='col-md-4 product-grid'>
 			<div class='image'>
-				<a href='../../sites/productsite/productsite.php?id=$phone->id'><img src='../../pictures/$phone->image' class='col-12'></a>
+				<a href='../../sites/adminproductsite/adminproductsite.php?id=$phone->id'><img src='../../pictures/$phone->image' class='col-12'></a>
 				<div class='overlay'>
 					<div class='edit'><a href='../../sites/editsite/editsite.php?id=$phone->id'><img src='../../pictures/rsz_edit.png'/></a></div>
 					<div class='delete'><a href='../../sites/deletesite/deletesite.php?id=$phone->id'><img src='../../pictures/rsz_cross.png'/></a></div>
-										<div class='detail'><a href='../../sites/productsite/productsite.php?id=$phone->id'></a></div>
+										<div class='detail'><a href='../../sites/adminproductsite/adminproductsite.php?id=$phone->id'></a></div>
 				</div>
 		</div>
 		<div id='items'>
 		<h5 class='text-center'>$phone->name</h5>
 		<h5 class='text-center'>$phone->price.$phone->brand</h5>
-		<a href='../../sites/productsite/productsite.php?id=$phone->id' class='btn buy'>Értékelések</a>
+		<a href='../../sites/adminproductsite/adminproductsite.php?id=$phone->id' class='btn buy'>Értékelések</a>
 		</div>
 	</div>";
 			}
