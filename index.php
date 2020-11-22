94c8fe00c8b8d06df73c62376571ef865b79ef7d
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2020-11-22 21:30:09 +0100

    index site content

--- index.php
+++ index.php
@@ -28,11 +28,6 @@
 		</ul>
 		
 	</div>
-	<div class="container">
-		<h1 class="text-center">Napló</h1>
-	<hr align="center">
-	<div class="row" align="center">
-		<div align="center">
 </div>
 	<?php
 	require("sites/indexsite/controller.php");
