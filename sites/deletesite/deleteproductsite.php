0dc05a9c1198bc6c5102dea8d9cb2ef36d8caeec
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2020-11-22 02:24:52 +0100

    delete added

---
+++ sites/deletesite/deleteproductsite.php
@@ -0,0 +1,36 @@
+<?php
+include ($_SERVER['DOCUMENT_ROOT'] ."/sites/loginsite/session.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Saturnus napló</title>
+	<link rel="stylesheet" type="text/css" href="../../style/style.css">
+	<link rel="stylesheet" type="text/css" href="../../bootstrap/css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+</head>
+<script type="text/javascript">
+	function toggleSidebar(){
+		document.getElementById("sidebar").classList.toggle('active');
+	}
+</script>
+<body>
+<div id="sidebar">
+		<div class="toggle-btn" onclick="toggleSidebar()">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<ul>
+			<a href="../../sites/loginsite/logout.php"><li>Kijelentkezés</li></a>
+		</ul>
+	</div>	</div>
+	<div class="row" align="center">
+	<?php
+	require($_SERVER['DOCUMENT_ROOT'] ."/sites/deletesite/deletesitecontroller.php");
+	$product = new DeleteProductController();
+	echo $product->ConfirmProduct();
+	?>
+</div>
+</body>
+</html>
--- sites/deletesite/deletesitecontroller.php
+++ sites/deletesite/deletesitecontroller.php
@@ -13,7 +13,7 @@
 				<hr>
 				<h5 class='text-center'>Osztály: $phone->price.$phone->brand</h5>
 		<h5 class='text-center'>Biztos törölni szeretné ezt a tanulót?</h5>
-		<form method='post' action='../../sites/adminsite/admin.php'>
+		<form method='post' action='../../sites/adminproductsite/adminproductsite.php?id=$grade->studentid'>
 		<a class='btn btn-secondary' href='../../sites/adminsite/admin.php' role='button'>Mégsem</a>
 		<button class='btn btn-danger' type='submit' value=$phone->id name='delete' href='../../sites/adminsite/admin.php' role='button'>Törlés</button>
 		</form>
@@ -22,6 +22,29 @@
 			}
 			return $result;
 		}
+		function ConfirmGrade(){
+			require ($_SERVER['DOCUMENT_ROOT'] ."/databaseaccess/accessdatabase.php");
+			$grade = new Grades();
+			$gradearray=$grade->GetGrades(pg_query($db_connectiontocatalog, "SELECT grades.id id, grades.date date, grades.grade grade, grades.subject subject, grades.studentid studentid FROM grades INNER JOIN phones ON phones.id = grades.studentid"));
+			$result ="";
+			$productid=$_GET['id'];
+			foreach ($gradearray as $key => $grade) {
+				if ($productid==$grade->id) {
+				$result=$result . "<div class='container'>
+				<h1 class='text-center'>$grade->subject</h1>
+				<hr>
+				<h5 class='text-center'>Érdemjegy: $grade->grade</h5>
+		<h5 class='text-center'>Biztos törölni szeretné ezt az érdemjegyet?</h5>
+		<form method='post' action='../../sites/adminsite/admin.php'>
+		<a class='btn btn-secondary' href='../../sites/adminproductsite/adminproductsite.php?id=$grade->studentid' role='button'>Mégsem</a>
+		<button class='btn btn-danger' type='submit' value=$grade->id name='delete' role='button'>Törlés</button>
+		</form>
+		</post>";
+			}
+			}
+			return $result;
+		
 		
 }
+	}
 ?>
